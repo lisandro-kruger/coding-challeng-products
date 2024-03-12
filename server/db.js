2253e4898692d62aa3f408da16b47732b74f83ba
lisandro-kruger <lisandro.kruger@outlook.es>
2024-03-11 22:25:59 -0300

    20240311

--- server/db.js
+++ server/db.js
@@ -1,17 +1,10 @@
-import {createPool} from 'mysql2/promise'
-import {
-  DB_HOST,
-  DB_NAME,
-  DB_PASSWORD,
-  DB_USER,
-  DB_PORT
-} from './config.js'
+import { Pool } from 'pg';
+import { config } from 'dotenv';
 
-export const pool = createPool({
-  user: DB_USER,
-  password: DB_PASSWORD,
-  host: DB_HOST,
-  port: DB_PORT,
-  database: DB_NAME
-})
+config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
